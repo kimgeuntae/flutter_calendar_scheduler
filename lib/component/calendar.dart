1797df697382aeae9a8558abb55968b58b7de3c6
kimgeuntae <kok617@naver.com>
2024-02-28 07:47:17 +0900

    Add: 캘린더 헤더 스타일

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -10,6 +10,14 @@ class Calendar extends StatelessWidget {
       focusedDay: DateTime.now(),
       firstDay: DateTime(1800),
       lastDay: DateTime(3009),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
     );
   }
 }
