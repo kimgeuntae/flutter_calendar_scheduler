1875231033b11969223aed956d7b1073df78842e
kimgeuntae <kok617@naver.com>
2024-02-28 07:57:46 +0900

    Add: 캘린더 일자 선택시 날짜 색상표시

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime? selectedDay;
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +25,20 @@ class Calendar extends StatelessWidget {
           fontSize: 16.0,
         ),
       ),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        print(selectedDay);
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime date) {
+        if (selectedDay == null) {
+          return false;
+        }
+        return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
+      },
     );
   }
 }
