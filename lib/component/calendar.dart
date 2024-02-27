96e98e8ddb1f7ca90d273d94a1f8459fa3a80430
kimgeuntae <kok617@naver.com>
2024-02-28 07:42:14 +0900

    Add: 테이블 캘린더 추가

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatelessWidget {
   const Calendar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1800),
+      lastDay: DateTime(3009),
+    );
   }
 }
