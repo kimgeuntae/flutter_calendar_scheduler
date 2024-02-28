5459f6548570692959ef16f612bcdacf70ee4411
kimgeuntae <kok617@naver.com>
2024-02-28 22:10:59 +0900

    Add: 상태관리를 부모위젝으로 올리기

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -2,16 +2,17 @@ import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class Calendar extends StatelessWidget {
+  final DateTime? selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
-  DateTime focusedDay = DateTime.now();
+  const Calendar({
+    required this.selectedDay,
+    required this.focusedDay,
+    required this.onDaySelected,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -59,14 +60,7 @@ class _CalendarState extends State<Calendar> {
           selectedTextStyle: defaultTextStyle.copyWith(
             color: PRIMARY_COLOR,
           )),
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        print(selectedDay);
-        setState(() {
-          this.selectedDay = selectedDay;
-          // 선택날짜를 포커스 데이트로 지정해줌으로써 해당 월로 이동 가능
-          this.focusedDay = selectedDay;
-        });
-      },
+      onDaySelected: onDaySelected,
       selectedDayPredicate: (DateTime date) {
         if (selectedDay == null) {
           return false;
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,8 +2,16 @@ import 'package:calendar_scheduler/component/calendar.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay;
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +19,12 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Calendar(),
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(height: 8.0),
             TodayBanner(
               selectedDay: DateTime.now(),
               scheduleCount: 3,
@@ -20,5 +33,13 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+      // 선택날짜를 포커스 데이트로 지정해줌으로써 해당 월로 이동 가능
+      this.focusedDay = selectedDay;
+    });
   }
 }
