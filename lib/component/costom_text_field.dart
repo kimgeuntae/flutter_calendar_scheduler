68369bb472a72e1c3782d9fc3cdae261289ade8c
kimgeuntae <kok617@naver.com>
2024-03-12 23:24:15 +0900

    Add: 텍스트 필드 커서 색상 추가

--- lib/component/costom_text_field.dart
+++ lib/component/costom_text_field.dart
@@ -6,6 +6,7 @@ class CostomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: Colors.grey, // 텍스트필드 커서 색상
       decoration: InputDecoration(
         border: InputBorder.none, // 텍스트 필드 아래줄 제거
         filled: true, // 해줘야 색상을 적용할 수 있음
