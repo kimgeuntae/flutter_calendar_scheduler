3aa5e122c749cd7390fc5b92f343b6a3ff5e2c8a
kimgeuntae <kok617@naver.com>
2024-03-12 23:12:56 +0900

    Mod: viewInsets을 패딩으로 줘서 키보드가 생길때 그만큼 패딩으로 바텀시트가 올라가게 구현

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -5,9 +5,22 @@ class ScheduleBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 시스템 UI 사이즈
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
     return Container(
-      height: 300.0,
+      height: MediaQuery.of(context).size.height / 2 + bottomInset,
       color: Colors.white,
+      child: Padding(
+        // bottomInset만큼 아래에 패딩을 줌
+        // 그러면 시스템 UI 사이즈가 생길때 자동으로 그만큼 패딩이 들어가서 bottomSheet 가 위로 올라감
+        padding: EdgeInsets.only(bottom: bottomInset),
+        child: Column(
+          children: [
+            TextField(),
+          ],
+        ),
+      ),
     );
   }
 }
